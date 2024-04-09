87ff3685ffd10257fab4823e976df5005e54c7cf
Max3796 <gurpreet.singh@kalvium.community>
2024-04-09 12:58:23 +0530

    Done

--- callback.js
+++ callback.js
@@ -1,13 +1,26 @@
 const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
 const newCookie = {name:"Biscotti Cookies"};
 
-
 // Progression 1: create a function to get all the cookies
+function getCookies(){
+  // Progression 2: using setTimeout() - use 1000 units for time parameter
+  setTimeout(()=>{
+    let output = ""
+  cookies.forEach(curr => {
+      output+= `<li>${curr.name}`
+    });
+    document.body.innerHTML = output
+  },1000)
+}
+//Progression 3: Create a function to create cookies
+function createCookies(callBack){
+  // use setTimeout() -- use 2000 units for time parameter
+  setTimeout(function(){
+    cookies.push(newCookie)
+    callBack()
+  },2000)
+}
 
-  // Progression 2: using setTimeout() 
-
-//Progression 3: Create a function to creat cookies
-
-
-
-// Progression 5: calling function
+// Progression 4: calling functions
+getCookies()
+createCookies(getCookies)
--- withoutCallback.js
+++ withoutCallback.js
@@ -2,10 +2,24 @@ const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
 const newCookie = {name:"Biscotti Cookies"};
 
 // Progression 1: create a function to get all the cookies
-
+function getCookies(){
   // Progression 2: using setTimeout() - use 1000 units for time parameter
-
-//Progression 3: Create a function to creat cookies
+  setTimeout(()=>{
+    let output = ""
+  cookies.forEach(curr => {
+      output+= `<li>${curr.name}`
+    });
+    document.body.innerHTML = output
+  },1000)
+}
+//Progression 3: Create a function to create cookies
+function createCookies(){
   // use setTimeout() -- use 2000 units for time parameter
+  setTimeout(function(){
+    cookies.push(newCookie)
+  },2000)
+}
 
 // Progression 4: calling functions
+getCookies()
+createCookies()
